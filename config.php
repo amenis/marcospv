99d879c5c7b508157696134f2053f7df65fec9cc
Alvaro Fabian <amenis10_10@hotmail.com>
2021-01-06 14:44:37 -0600

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-define("urlsite",'http://localhost/mvc/');
+define("urlsite",'http://localhost/marcospv/');
 define("defaultCtl","Login");
 define("defaultAtion","index");
 define("host","127.0.0.1");
